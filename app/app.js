6e95e6ffb281e4e113e8f242af573045631fb8fb
Alejandro Medina <5almero5@gmail.com>
2016-07-17 18:34:43 -0500

    initial comit Dev enviromet

---
+++ app/app.js
@@ -0,0 +1,10 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+//import components
+import App from './components/Main';
+//css
+import css from './styles/app.scss';
+
+
+render(<App/>, document.getElementById('root'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './app/app'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+    // js
+    {
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'app')
+    },
+    // CSS
+    {
+      test: /\.scss$/,
+      include: path.join(__dirname, 'app'),
+      loader: 'style-loader!css-loader!sass-loader'
+    }
+    ]
+  }
+};
